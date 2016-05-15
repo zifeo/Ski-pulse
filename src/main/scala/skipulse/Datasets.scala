1ee40db420bbeaab6631488269c94ec6d9be27fc
Teo Stocco <teo.stocco@epfl.ch>
2016-05-15 04:10:59 +0200

    remove chable

--- src/main/scala/skipulse/Datasets.scala
+++ src/main/scala/skipulse/Datasets.scala
@@ -11,11 +11,13 @@ import scala.io.{Codec, Source}
 
 case class ModelData(station: String, date: DateTime, skier: Int)
 case class TicketingData(station: String, date: DateTime, count: Int)
-case class Station(name: String, startX: Int, startY: Int, endX: Int, endY: Int, capacity: Int, flow: Int)
+case class Station(name: String, startX: Int, startY: Int, endX: Int, endY: Int, capacity: Int, flow: Int, deniv: Int, length: Int)
 
 object Datasets extends App {
 
   import DefaultJsonProtocol._
+
+  implicit val stationF = jsonFormat9(Station)
 
   val folder = "datasets"
   val datetimeF = DateTimeFormat.forPattern("dd.MM.yy HH:mm:ss")
@@ -27,8 +29,8 @@ object Datasets extends App {
     line <- source("positions_remontees.rmt")
   } yield
     line.split(' ') match {
-      case Array(_, name, sx, sy, ex, ey, cap, flo) =>
-        Station(name, sx.toInt, sy.toInt, ex.toInt, ey.toInt, cap.toInt, flo.toInt)
+      case Array(_, name, sx, sy, ex, ey, cap, flo, deniv, len) =>
+        Station(name, sx.toInt, sy.toInt, ex.toInt, ey.toInt, cap.toInt, flo.toInt, deniv.toInt, len.toInt)
     }
 
   val model = for {
@@ -112,6 +114,8 @@ object Datasets extends App {
 
   val min = ticketing.map(_.date).min
 
+  assert(stations.map(_.name).distinct.sorted == ticketing.map(_.station).distinct.sorted)
+
   val output =
     ticketing
       .groupBy(x => (x.date.getDayOfYear + min.getDayOfYear) % 365)
@@ -135,13 +139,13 @@ object Datasets extends App {
 
   val str =
     JsObject(
-      //"stations" -> output.toJson,
+      "stations" -> stations.toJson,
       "ticketing" -> output.toJson
     )
 
 
   val js = new PrintWriter(new File("static/ds.json"))
-js.write(str.compactPrint)
-  js.close
+  js.write(str.compactPrint)
+  js.close()
 
 }
